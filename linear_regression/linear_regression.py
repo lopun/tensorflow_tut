e7bbad9eadec082247ae5a40fd73f80a81648073
ujin43255252 <ujin43255252@gmail.com>
2018-07-23 12:27:15 +0900

    linear_regression & multivariables'

---
+++ linear_regression/linear_regression.py
@@ -0,0 +1,27 @@
+import tensorflow as tf
+import matplotlib.pyplot as plt
+
+x_train = [1,2,3]
+y_train = [2,3,4]
+
+W = tf.placeholder(tf.float32)
+
+hypothesis = x_train * W
+
+cost = tf.reduce_mean(tf.square(hypothesis - y_train))
+
+sess = tf.Session()
+
+sess.run(tf.global_variables_initializer())
+
+W_val = []
+cost_val = []
+
+for i in range(-30, 50):
+    feed_W = i * 0.1
+    curr_cost, curr_W = sess.run([cost, W], feed_dict = {W: feed_W})
+    W_val.append(curr_W)
+    cost_val.append(curr_cost)
+
+plt.plot(W_val, cost_val)
+plt.show()
